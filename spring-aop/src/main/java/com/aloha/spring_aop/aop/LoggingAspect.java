a8836deca073196d3d0a780c9ad241ceb3389415
wwwalohacampus <h850415@naver.com>
2026-01-06 12:23:55 +0900

    AOP - 2

--- spring-aop/src/main/java/com/aloha/spring_aop/aop/LoggingAspect.java
+++ spring-aop/src/main/java/com/aloha/spring_aop/aop/LoggingAspect.java
@@ -1,10 +1,13 @@
 package com.aloha.spring_aop.aop;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -23,7 +26,7 @@ public class LoggingAspect {
   // ⭐ Advice
   // ⚡ Point Cut  : execution( 접근제한자 반환타입 패키지.클래스.메서드(파라미터) )
   // ⚡ Join Point : @Before, @After, @Around 등
-  @Before("execution(* com.aloha.spring_aop.service.BoardService*.*(..))")
+  // @Before("execution(* com.aloha.spring_aop.service.BoardService*.*(..))")
   public void before(JoinPoint jp) {
     // jp.getSignature() : 타겟 메소드 시그니처 정보(반환타입, 패키지.클래스.메소드) 반환
     // jp.getArgs()      : 타겟 메소드의 매개변수를 반환
@@ -38,7 +41,7 @@ public class LoggingAspect {
   }
 
 
-  @After("execution(* com.aloha.spring_aop.service.BoardService*.*(..))")
+  // @After("execution(* com.aloha.spring_aop.service.BoardService*.*(..))")
   public void after(JoinPoint jp) {
     // jp.getSignature() : 타겟 메소드 시그니처 정보(반환타입, 패키지.클래스.메소드) 반환
     // jp.getArgs()      : 타겟 메소드의 매개변수를 반환
@@ -51,6 +54,43 @@ public class LoggingAspect {
     printParam(jp);
     log.info("=============================================");
   }
+
+
+  /**
+   * @Around 조인포인트를 적용하면, @After 어드바이스는 실행되지 않는다.
+   * (Around 에서 직접 after 를 호츨하여 실행시킨다.)
+   * 
+   * ProceedingJoinPoint : 어드바이스에서 타겟 메소드의 실행을 제어하고 호출하는 객체
+   * - proceed()         : 타겟 메소드를 호출
+   * @param jp
+   * @return
+   */
+  @Around("execution(* com.aloha.spring_aop.service.BoardService*.*(..))")
+	public Object around(ProceedingJoinPoint jp) {
+		// jp.getSignature() : 타겟 메소드의 시그니처 정보(반환타입, 패키지.클래스.메소드) 반환
+		// jp.getArgs()		 : 타겟 메소드의 매개변수를 반환
+		log.info("==================================================");
+		log.info("[@Around] ########################################");
+		log.info("target : " + jp.getTarget().toString());
+		log.info("signature : " + jp.getSignature());
+		log.info("args : " + Arrays.toString(jp.getArgs()));
+		LocalDateTime time = LocalDateTime.now();
+		log.info("현재 시간 : " + time);
+		
+		Object result = null;
+		try {
+			result = jp.proceed();      // 타겟 메소드 호출
+			if( result != null )
+				log.info("반환값 : " + result.toString());
+		} catch (Throwable e) {
+			log.error("예외가 발생하였습니다.");
+			e.printStackTrace();
+		}
+		// after(jp);                    // @After 어드바이스 직접 호출
+		log.info("==================================================");
+		return result;
+	}
+
 
   /**
    * 파라미터 출력
