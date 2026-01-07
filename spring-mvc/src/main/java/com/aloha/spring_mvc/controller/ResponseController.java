fadb3a831e710a5a7ebc664c5263731b72918aef
wwwalohacampus <h850415@naver.com>
2026-01-07 17:40:02 +0900

    MVC 요청처리

--- spring-mvc/src/main/java/com/aloha/spring_mvc/controller/ResponseController.java
+++ spring-mvc/src/main/java/com/aloha/spring_mvc/controller/ResponseController.java
@@ -1,6 +1,5 @@
 package com.aloha.spring_mvc.controller;
 
-import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
@@ -13,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
--- spring-mvc/src/main/java/com/aloha/spring_mvc/dto/Board.java
+++ spring-mvc/src/main/java/com/aloha/spring_mvc/dto/Board.java
@@ -1,24 +1,31 @@
 package com.aloha.spring_mvc.dto;
 
 import java.util.Date;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Board {
-  private final Long no;
-  private final String title;
-  private final String writer;
-  private final String content;
+  private Long no;
+  private String title;
+  private String writer;
+  private String content;
   private Date createdAt;
   private Date updatedAt;
 
-  // public Board(Long no, String title, String writer, String content) {
-	// 	this.no = no;
-	// 	this.title = title;
-	// 	this.writer = writer;
-	// 	this.content = content;
-	// }
+  // 파일 데이터
+  // private MultipartFile[] fileList;
+  private List<MultipartFile> fileList;
+
+  public Board(Long no, String title, String writer, String content) {
+		this.no = no;
+		this.title = title;
+		this.writer = writer;
+		this.content = content;
+	}
 }
